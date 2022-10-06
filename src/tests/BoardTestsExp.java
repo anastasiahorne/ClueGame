a83b87ac9826b89444fb57a114c5ae7d87b66ff0
Rachel Jones <racheljones@mines.edu>
2022-10-05 21:35:05 -0600

    Added tests package

---
+++ src/tests/BoardTestsExp.java
@@ -0,0 +1,67 @@
+package tests;
+
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+
+import experiment.TestBoard;
+import experiment.TestBoardCell;
+
+class BoardTestsExp {
+	TestBoard board;
+	
+	// @BeforeEach method to set up your BoardExp
+	@BeforeEach
+	public void setUp() {
+		// board should create adjacency list
+		board = new TestBoard();
+	}
+	
+	/*
+	 * Test adjacencies for several different locations
+	 * Test centers and edges
+	 */
+	@Test
+	public void testAdjacency() {
+		TestBoardCell cell = board.getCell(0, 0);
+		Set<TestBoardCell> testList = cell.getAdjList();
+		Assert.assertTrue(testList.contains(board.getCell(1, 0)));
+		Assert.assertTrue(testList.contains(board.getCell(0, 1)));
+		Assert.assertEquals(2, testList.size());
+	}
+	
+	/*
+	 * Test targets with several rolls and start locations
+	 */
+	@Test
+	public void testTargetsNormal() {
+		TestBoardCell cell = board.getCell(0, 0);
+		board.calcTargets(cell, 3);
+		Set<TestBoardCell> targets = board.getTargets();
+		Assert.assertEquals(6,  targets.size());
+		Assert.assertTrue(targets.contains(board.getCell(3, 0)));
+		Assert.assertTrue(targets.contains(board.getCell(2, 1)));
+		Assert.assertTrue(targets.contains(board.getCell(0, 1)));
+		Assert.assertTrue(targets.contains(board.getCell(1, 2)));
+		Assert.assertTrue(targets.contains(board.getCell(0, 3)));
+		Assert.assertTrue(targets.contains(board.getCell(1, 0)));
+	}
+	
+	@Test
+	public void testTargetsMixed() {
+		// set up occupied cells
+		board.getCell(0, 2).setOccupied(true);
+		board.getCell(1, 2).setRoom(true);
+		TestBoardCell cell = board.getCell(0, 3);
+		board.calcTargets(cell, 3);
+		Set<TestBoardCell> targets = board.getTargets();
+		Assert.assertEquals(3,  targets.size());
+		Assert.assertTrue(targets.contains(board.getCell(1, 2)));
+		Assert.assertTrue(targets.contains(board.getCell(2, 2)));
+		Assert.assertTrue(targets.contains(board.getCell(3, 3)));
+	}
+}
