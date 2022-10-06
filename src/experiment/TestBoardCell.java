c0548f7f0dd6587c8df5f952e87a1974ebd52cca
Rachel Jones <racheljones@mines.edu>
2022-10-05 20:20:27 -0600

    TestBoardCell constructor added

--- src/experiment/TestBoardCell.java
+++ src/experiment/TestBoardCell.java
@@ -5,6 +5,13 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class TestBoardCell {
+	int row;
+	int column;
+	
+	public TestBoardCell(int r, int c) {
+		row = r;
+		column = c;
+	}
 
 	@Test
 	void test() {
