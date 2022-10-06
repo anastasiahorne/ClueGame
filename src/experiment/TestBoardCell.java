0b2292f7cee65c29b0ae3c780b7381c262513562
Rachel Jones <racheljones@mines.edu>
2022-10-05 20:38:50 -0600

    addAdjacency method added to TestBoardCell

--- src/experiment/TestBoardCell.java
+++ src/experiment/TestBoardCell.java
@@ -2,15 +2,24 @@ package experiment;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Set;
+
 import org.junit.jupiter.api.Test;
 
 class TestBoardCell {
 	int row;
 	int column;
+	Set<TestBoardCell> adjList;
 	
+	// Default constructor
 	public TestBoardCell(int r, int c) {
 		row = r;
 		column = c;
+	}
+	
+	// Setter to add a cell to the adjacency list
+	public void addAdjacency(TestBoardCell cell) {
+		adjList.add(cell);
 	}
 
 	@Test
