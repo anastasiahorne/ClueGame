0583966c4660b4b4ea5e195a4cd76a55d02bf859
Anastasia Horne <anastasiahorne@mines.edu>
2022-10-10 10:01:48 -0600

    added instance variables to BoardCell and the types to DoorDirection

--- src/clueGame/BoardCell.java
+++ src/clueGame/BoardCell.java
@@ -1,5 +1,19 @@
 package clueGame;
 
-public class BoardCell {
+import java.util.Set;
 
+public class BoardCell {
+	private int row;
+	private int col;
+	private char initial;
+	private DoorDirection doorDirection;
+	private boolean roomLabel;
+	private boolean roomCenter;
+	private char secretPassge;
+	private Set<BoardCell> adjList;
+	
+	public void addAdj(BoardCell adj) {
+		adjList.add(adj);
+	}
+	
 }
