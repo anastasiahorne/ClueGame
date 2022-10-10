95d1c185d2d696ebae87c12521a13b71eb536053
Anastasia Horne <anastasiahorne@mines.edu>
2022-10-10 08:48:16 -0600

    added preliminary instance variables and method delcarations to board class

--- src/clueGame/Board.java
+++ src/clueGame/Board.java
@@ -1,5 +1,29 @@
 package clueGame;
 
+import java.util.Map;
+
 public class Board {
 
+	private BoardCell[][] grid;
+	private int numRows;
+	private int numColumns;
+	private String layoutConfigFile;
+	private String setupConfigFile;
+	private Map<Character,Room> roomMap;
+	private static Board theInstance;
+	
+	public void initialize() {
+		
+	}
+	//this should throw badconfig exception
+	public void loadSetupConfig() {
+		
+	}
+	//this should throw badconfig exception
+	public void loadLayoutConfig() {
+		
+	}
+	public static Board getInstance() {
+		return theInstance;
+	}
 }
