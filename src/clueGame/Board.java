df028901c2791ecfa43ca1861f5234f3f2ec1aeb
Anastasia Horne <anastasiahorne@mines.edu>
2022-10-10 11:13:20 -0600

    read in settup file and populate our map with the names and characters

--- src/clueGame/Board.java
+++ src/clueGame/Board.java
@@ -1,7 +1,11 @@
 package clueGame;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
+import java.util.Scanner;
 
 import experiment.TestBoardCell;
 
@@ -12,7 +16,7 @@ public class Board {
 	private int numColumns;
 	private String layoutConfigFile;
 	private String setupConfigFile;
-	private Map<Character,Room> roomMap;
+	private Map<Character, String> roomMap;
 	   /*
      * variable and methods used for singleton pattern
      */
@@ -29,6 +33,7 @@ public class Board {
       * initialize the board (since we are using singleton pattern)
       */
      public void initialize(){
+    	 roomMap=new HashMap<>();
     	 grid = new BoardCell[numRows][numColumns];
  		for (int i = 0; i < numRows; i++) {
  			for (int j = 0; j < numColumns; j++) {
@@ -36,9 +41,34 @@ public class Board {
  			}
  		}
      }
+     
+ 	public void setLayoutConfigFile(String layoutConfigFile) {
+ 		this.layoutConfigFile = layoutConfigFile;
+ 	}
+ 	public void setSetupConfigFile(String setupConfigFile) {
+ 		this.setupConfigFile = setupConfigFile;
+ 	}
 	
 	//this should throw badconfig exception
-	public void loadSetupConfig() {
+	public void loadSetupConfig() throws FileNotFoundException{
+		FileReader reader=new FileReader(setupConfigFile);
+		Scanner in=new Scanner(reader);
+		char character;
+		String roomName;
+		String Type;
+		while (in.hasNextLine()) {
+			String line=in.nextLine();
+			try {
+				if (line.charAt(0)== '/') {
+					in.useDelimiter(", ");
+					Type=in.next();
+					roomName=in.next();
+					character=in.nextLine().charAt(0);
+					roomMap.put(character,roomName);
+				}
+			}
+			catch(FileNotFoundException e) {
+		}
 		
 	}
 	//this should throw badconfig exception
--- src/clueGame/BoardCell.java
+++ src/clueGame/BoardCell.java
@@ -11,6 +11,8 @@ public class BoardCell {
 	private boolean roomCenter;
 	private char secretPassge;
 	private Set<BoardCell> adjList;
+	private boolean isRoom;
+	private boolean isOccupied;
 	
 	public BoardCell(int i, int j) {
 		// TODO Auto-generated constructor stub
@@ -19,5 +21,25 @@ public class BoardCell {
 	public void addAdj(BoardCell adj) {
 		adjList.add(adj);
 	}
+	// Indicates that a cell is part of a room
+		public void setIsRoom(boolean room) {
+			isRoom = room;
+		}
+		
+		// Get whether or not a cell is part of a room
+		public boolean getIsRoom() {
+			return isRoom;
+		}
+		
+		// Setter for indicating a cell is occupied by another player
+		public void setIsOccupied(boolean occupied) {
+			isOccupied = occupied;
+		}
+		
+		// Getter for indicating a cell is occupied by another player
+		public boolean getIsOccupied() {
+			return isOccupied;
+		}
+
 	
 }
