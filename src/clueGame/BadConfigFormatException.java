75bcb1f1bde3ed675ed065dced7f1022eda9f16d
Rachel Jones <racheljones@mines.edu>
2022-10-10 18:57:28 -0600

    BadConfigFormatException changed to extend RuntimeException and constructor added

--- src/clueGame/BadConfigFormatException.java
+++ src/clueGame/BadConfigFormatException.java
@@ -1,5 +1,7 @@
 package clueGame;
 
-public class BadConfigFormatException extends Exception {
-
+public class BadConfigFormatException extends RuntimeException {
+	public BadConfigFormatException (String message, Throwable error) {
+		super(message, error);
+	}
 }
