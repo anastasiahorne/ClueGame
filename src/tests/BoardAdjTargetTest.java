ccc6d5d007e3277c6539d871f5f84bfebca16bf1
Rachel Jones <racheljones@mines.edu>
2022-10-13 23:21:45 -0600

    testTargetsInMarquez updated to reflect spreadsheet

--- src/tests/BoardAdjTargetTest.java
+++ src/tests/BoardAdjTargetTest.java
@@ -151,31 +151,42 @@ class BoardAdjTargetTest {
 		}
 		
 		@Test
-		public void testTargetsInKitchen() {
+		public void testTargetsInMarquez() {
 			// test a roll of 1
-			board.calcTargets(board.getCell(20, 19), 1);
+			board.calcTargets(board.getCell(3, 22), 1);
 			Set<BoardCell> targets= board.getTargets();
 			assertEquals(2, targets.size());
-			assertTrue(targets.contains(board.getCell(17, 18)));
-			assertTrue(targets.contains(board.getCell(2, 2)));	
+			assertTrue(targets.contains(board.getCell(6, 20)));
+			assertTrue(targets.contains(board.getCell(0, 24)));	
 			
 			// test a roll of 3
-			board.calcTargets(board.getCell(20, 19), 3);
+			board.calcTargets(board.getCell(3, 22), 3);
 			targets= board.getTargets();
 			assertEquals(6, targets.size());
-			assertTrue(targets.contains(board.getCell(17, 20)));
-			assertTrue(targets.contains(board.getCell(16, 19)));	
-			assertTrue(targets.contains(board.getCell(17, 16)));
-			assertTrue(targets.contains(board.getCell(2, 2)));	
+			assertTrue(targets.contains(board.getCell(18, 4)));
+			assertTrue(targets.contains(board.getCell(5, 19)));	
+			assertTrue(targets.contains(board.getCell(7, 21)));
+			assertTrue(targets.contains(board.getCell(7, 19)));	
+			assertTrue(targets.contains(board.getCell(1, 19)));
+			assertTrue(targets.contains(board.getCell(5, 19)));
 			
 			// test a roll of 4
-			board.calcTargets(board.getCell(20, 19), 4);
+			board.calcTargets(board.getCell(3, 22), 4);
 			targets= board.getTargets();
-			assertEquals(9, targets.size());
-			assertTrue(targets.contains(board.getCell(16, 18)));
-			assertTrue(targets.contains(board.getCell(18, 16)));	
-			assertTrue(targets.contains(board.getCell(16, 16)));
-			assertTrue(targets.contains(board.getCell(2, 2)));	
+			assertEquals(13, targets.size());
+			assertTrue(targets.contains(board.getCell(16, 4)));
+			assertTrue(targets.contains(board.getCell(17, 5)));
+			assertTrue(targets.contains(board.getCell(18, 6)));
+			assertTrue(targets.contains(board.getCell(19, 5)));
+			assertTrue(targets.contains(board.getCell(19, 3)));
+			assertTrue(targets.contains(board.getCell(7, 22)));
+			assertTrue(targets.contains(board.getCell(8, 21)));
+			assertTrue(targets.contains(board.getCell(4, 19)));
+			assertTrue(targets.contains(board.getCell(7, 18)));
+			assertTrue(targets.contains(board.getCell(8, 19)));
+			assertTrue(targets.contains(board.getCell(7, 20)));
+			assertTrue(targets.contains(board.getCell(0, 19)));
+			assertTrue(targets.contains(board.getCell(6, 19)));
 		}
 
 		// Tests out of room center, 1, 3 and 4
