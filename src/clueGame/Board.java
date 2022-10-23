421e66bbb571cb41630bc2894994a2ff16bd0e8e
Rachel Jones <racheljones@mines.edu>
2022-10-23 14:22:42 -0600

    Added commenting to methods in Board and BoardCell

--- src/clueGame/Board.java
+++ src/clueGame/Board.java
@@ -241,6 +241,7 @@ public class Board {
 		
 	}
 	
+	// Clear sets and make a call to findAllTargets
 	public void calcTargets(BoardCell startCell, int pathLength) {
 		visited.clear();
 		targets.clear();
@@ -249,6 +250,7 @@ public class Board {
 		targets.remove(startCell);
 	}
 	
+	// Get all cells that are pathLength cells away from thisCell
 	public void findAllTargets(BoardCell thisCell, int numSteps) {
 		for (BoardCell adjCell: thisCell.getAdjList()) {
 			if (adjCell.getIsRoom()) {
