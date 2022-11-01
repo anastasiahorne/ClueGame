2a1b00a279e7e08238242b9c7b53e67750ed5c78
Rachel Jones <racheljones@mines.edu>
2022-11-01 13:36:21 -0600

    Added abstract method to Player class

--- src/tests/GameSetupTests.java
+++ src/tests/GameSetupTests.java
@@ -5,10 +5,17 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class GameSetupTests {
-
+	// Add BeforeAll
+	
+	// Test that all people are loaded in
 	@Test
-	void test() {
+	void testPlayersLoaded() {
 		fail("Not yet implemented");
 	}
+	
+	// Proper Human or Computer player is initialized based on people data
+	// Deck of all cards is created (composed of rooms, weapons, and people)
+	// The solution to the game is dealt
+	// The other cards are dealt to the players.
 
 }
