8f60b3d2aeb737d042c9c823e953c93842c44fdc
Rachel Jones <racheljones@mines.edu>
2022-11-08 13:24:11 -0700

    Added code to constructor in GameControlPanel

--- src/clueGame/GameControlPanel.java
+++ src/clueGame/GameControlPanel.java
@@ -12,12 +12,29 @@ import javax.swing.border.EtchedBorder;
 import javax.swing.border.TitledBorder;
 
 public class GameControlPanel extends JPanel {
-
+	private JTextField guess;
+	
 	/**
 	 * Constructor for the panel, it does 90% of the work
 	 */
 	public GameControlPanel()  {
-		
+		setLayout(new GridLayout(2,0));
+		JPanel panel = createGuessPanel("Guess");
+		add(panel);
+		panel = createGuessPanel("Guess Result");
+		add(panel);
+	}
+	
+	private JPanel createGuessPanel(String title) {
+		JPanel panel = new JPanel();
+		// Use a grid layout, 1 row, 2 elements (label, text)
+		panel.setLayout(new GridLayout(1,0));
+		JLabel nameLabel = new JLabel("Name");
+		guess = new JTextField(20);
+		panel.add(nameLabel);
+		panel.add(guess);
+		panel.setBorder(new TitledBorder (new EtchedBorder(), title));
+		return panel;
 	}
 	
 	/**
@@ -27,6 +44,7 @@ public class GameControlPanel extends JPanel {
 	 */
 	public static void main(String[] args) {
 		GameControlPanel panel = new GameControlPanel();  // create the panel
+		panel.setLayout(new GridLayout(2,0));
 		JFrame frame = new JFrame();  // create the frame 
 		frame.setContentPane(panel); // put the panel in the frame
 		frame.setSize(750, 180);  // size the frame
@@ -39,7 +57,7 @@ public class GameControlPanel extends JPanel {
 		panel.setGuess( "So you have nothing?");
 	}
 
-	private void setTurn(ComputerPlayer computerPlayer, int i) {
+	private void setTurn(Player player, int roll) {
 		// TODO Auto-generated method stub
 		
 	}
