ce9acd339ea61e8b3ba3ed5ff5501d9334bffc30
Rachel Jones <racheljones@mines.edu>
2022-11-10 13:04:34 -0700

    Added constructor to KnownCardsPanel

--- src/clueGame/KnownCardsPanel.java
+++ src/clueGame/KnownCardsPanel.java
@@ -3,5 +3,22 @@ package clueGame;
 import javax.swing.JPanel;
 
 public class KnownCardsPanel extends JPanel {
-
+	
+	// Default constructor
+	public KnownCardsPanel() {
+		
+	}
+	
+	public static void main(String[] args) {
+		Board testBoard = Board.getInstance();
+		
+		testBoard.setConfigFiles("ClueLayout.csv", "ClueSetup.txt");
+		
+		testBoard.initialize();
+		
+		// mark all cards as seen for testing
+		for () {
+			
+		}
+	}
 }
